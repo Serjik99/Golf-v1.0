0711aa59fe5e63623e7a5747069927016b884e5e
FerrusManus <roma.hlinin@yandex.ru>
2021-06-27 03:10:35 +0300

    SeeYa

---
+++ GolfServ/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GolfServ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
